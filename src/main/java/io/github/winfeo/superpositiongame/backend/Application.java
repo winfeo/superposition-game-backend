b1b2278f7b810ae2760ddec9bd02cd84979c1651
winfeo <kirill.ryabikin.05@gmail.com>
2026-06-13 20:18:22 +0300

    config: Подключение к удалённой БД, настройка local. и remote.properties файлов для секретов

--- src/main/java/io/github/winfeo/superpositiongame/backend/Application.java
+++ src/main/java/io/github/winfeo/superpositiongame/backend/Application.java
@@ -1,16 +1,11 @@
 package io.github.winfeo.superpositiongame.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("JWT_KEY", dotenv.get("JWT_KEY"));
-        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-
         SpringApplication.run(Application.class, args);
 	}
 
