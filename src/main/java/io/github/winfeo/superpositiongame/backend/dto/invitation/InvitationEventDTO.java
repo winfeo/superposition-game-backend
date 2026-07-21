740a69c1fc5f7b9255651959c2e4d693b0e26702
winfeo <kirill.ryabikin.05@gmail.com>
2026-07-21 22:12:14 +0300

    fix: Отправка списка приглашений при инициализации

--- src/main/java/io/github/winfeo/superpositiongame/backend/dto/invitation/InvitationEventDTO.java
+++ src/main/java/io/github/winfeo/superpositiongame/backend/dto/invitation/InvitationEventDTO.java
@@ -1,6 +1,26 @@
 package io.github.winfeo.superpositiongame.backend.dto.invitation;
 
+import java.util.List;
+
 public record InvitationEventDTO(
-    InvitationEventType type,
-    InvitationDTO invitation
-) { }
+        InvitationEventType type,
+        InvitationDTO invitation,
+        List<InvitationDTO> invitations
+) {
+    public InvitationEventDTO(
+            InvitationEventType type,
+            InvitationDTO invitation
+    ) {
+        this(type, invitation, null);
+    }
+
+    public static InvitationEventDTO initial(
+            List<InvitationDTO> invitations
+    ) {
+        return new InvitationEventDTO(
+                InvitationEventType.INIT,
+                null,
+                invitations
+        );
+    }
+}
